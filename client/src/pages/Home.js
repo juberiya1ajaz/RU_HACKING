0d6791c516d9e19147210b672a4d4fb918276440
utkarsh575 <utkarshlohani575@gmail.com>
2022-06-26 14:00:14 +0530

    :lipstick: added Donate Page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,37 @@
-import React from "react"
-import HeroImg from '../assets/hero.svg'
-import FeatImg from '../assets/feat.svg'
-import { Link } from 'react-router-dom';
+import React from "react";
+import HeroImg from "../assets/hero.svg";
+import FeatImg from "../assets/feat.svg";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-
   return (
-    <div className='md:mx-28 mx-4 text-white pt-10 pb-12'>
-
-      <div className='md:grid md:grid-cols-2 items-center pt-10'>
-        <div className=''>
-          <h1 className='text-3xl md:text-6xl'>Who we are</h1>
-          <p className='text-xl py-4 tracking-wider text-justify'>About the app.</p>
+    <div className="md:mx-28 mx-4 text-white pt-10 pb-12">
+      <div className="md:grid md:grid-cols-2 items-center pt-10">
+        <div className="">
+          <h1 className="text-3xl md:text-6xl">Who we are</h1>
+          <p className="text-xl py-4 tracking-wider text-justify">
+            About the app.
+          </p>
 
           <Link to="/browse">
-            <button className='bg-secondary py-2 px-8 rounded-md text-xl md:text-2xl'>Browse</button>
+            <button className="bg-secondary py-2 px-8 rounded-md text-xl md:text-2xl">
+              Browse
+            </button>
           </Link>
-
         </div>
         <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
           <img src={HeroImg} alt="img" width="500" height="500" />
         </div>
       </div>
 
-      <div className='md:grid md:grid-cols-2 pt-12 items-center'>
+      <div className="md:grid md:grid-cols-2 pt-12 items-center">
         <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
           <img src={FeatImg} alt="img" width="400" height="400" />
         </div>
-        <div className=''>
-          <h1 className='text-3xl md:text-5xl'>What else do we have</h1>
-          <p className='text-xl md:text-2xl py-4 tracking-wider'>APP NAME is a web app where you can:
+        <div className="">
+          <h1 className="text-3xl md:text-5xl">What else do we have</h1>
+          <p className="text-xl md:text-2xl py-4 tracking-wider">
+            ShareNcare is a web app where you can:
           </p>
           <ul className="text-xl">
             <li className="list-disc">Exchange items with others.</li>
@@ -37,5 +39,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-  )
+  );
 }
